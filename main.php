5c9a86fc0312e3c96aad700d874f599a3ab6d2ef
florentinokinlaurence-hash <florentinokinlaurence@smcbi.edu.ph>
2025-11-03 20:00:48 +0800

    Create main.php

---
+++ main.php
@@ -0,0 +1,4 @@
+Part IV — Integration Challenge (main.php)
+
+Combine all three parts into one integrated system.
+You can add a search bar for users to search for a book title, and display a ✅ if found or ❌ if not.
